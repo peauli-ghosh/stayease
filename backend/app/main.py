67ad01bc859efb694141671bc974be1b4c8d82c4
Peauli Ghosh <peauli231001102272@technoindiaeducation.com>
2026-03-19 20:50:59 +0530

    Task 02 Step 4: Database initialized and tables created

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from app.routes.user_routes import router as user_router
 
+from app.db.database import engine, Base
+from app.models import user_model
+
 app = FastAPI()
+
+# Create database tables
+Base.metadata.create_all(bind=engine)
 
 # Include user routes
 app.include_router(user_router)
